6c67d7d56471e181dd267c3cf4f94b852ec40e79
yangjian <362961910@qq.com>
2022-11-07 21:13:48 +0800

    优化

--- free-tunnel-client/src/main/java/cn/t/freetunnel/client/socks5/handler/Socks5TunnelClientForwardingHandler.java
+++ free-tunnel-client/src/main/java/cn/t/freetunnel/client/socks5/handler/Socks5TunnelClientForwardingHandler.java
@@ -1,13 +1,12 @@
 package cn.t.freetunnel.client.socks5.handler;
 
 import cn.t.freetunnel.common.handler.ForwardingMessageHandler;
-import io.netty.buffer.Unpooled;
+import cn.t.freetunnel.common.util.TunnelUtil;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 
 /**
- * 消息转发处理器(右 -> 左)
+ * 通道客户端消息转发处理器(右 -> 左)
  *
  * @author <a href="mailto:jian.yang@liby.ltd">野生程序员-杨建</a>
  * @version V1.0
@@ -19,7 +18,7 @@ public class Socks5TunnelClientForwardingHandler extends ForwardingMessageHandle
     public void channelInactive(ChannelHandlerContext ctx) {
         if(remoteChannel.isOpen()) {
             logger.info("[{} -> {}]: 服务端通道断开, 关闭客户端连接: [{} -> {}]", ctx.channel().localAddress(), ctx.channel().remoteAddress(), remoteChannel.remoteAddress(), remoteChannel.localAddress());
-            remoteChannel.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+            TunnelUtil.closeGracefully(ctx.channel());
         } else {
             logger.info("[{} -> {}]: 断开连接", ctx.channel().localAddress(), ctx.channel().remoteAddress());
         }
